2bfa4adcc76cca820d56b11cd796399732ac9bd4
danntracey329 <danntracey@icloud.com>
2020-10-20 21:37:02 +0100

    main.css and styling

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Dan Tracey",
+        "title": "First Post",
+        "content": "First content",
+        "date_posted": "October 19, 2020",
+    },
+    {
+        "author": "Lily Tracey",
+        "title": "Second Post",
+        "content": "Second content",
+        "date_posted": "October 20, 2020",
+    },
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {'title': 'About'})
