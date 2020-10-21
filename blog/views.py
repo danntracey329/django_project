508886fa6854237f83dbe2641ddf4b81a9a46e43
danntracey329 <danntracey@icloud.com>
2020-10-21 10:48:53 +0100

    Database and admin page setup

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        "author": "Dan Tracey",
-        "title": "First Post",
-        "content": "First content",
-        "date_posted": "October 19, 2020",
-    },
-    {
-        "author": "Lily Tracey",
-        "title": "Second Post",
-        "content": "Second content",
-        "date_posted": "October 20, 2020",
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {
-        'posts': posts
-    }
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html", {'title': 'About'})
+    return render(request, "blog/about.html", {"title": "About"})
